b7f6a15d3be6847fbf807d478d5b2c526c319fc7
adevadiga <abhinandhan.d@gmail.com>
2022-04-14 18:38:12 +0200

    Initial commit

---
+++ components/Tutorial.vue
@@ -0,0 +1,26 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+      <div class="box mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
+        <img src="https://c.tenor.com/aD7y5Tg9BzwAAAAC/web-design-modern-web-gif.gif" alt="">
+        <h2 class="text-2xl leading-7 font-semibold">
+          Toolabs is loading...
+        </h2>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial'
+}
+</script>
+<style scoped>
+.box{
+  padding: 3rem;
+  text-align: center;
+}
+</style>
