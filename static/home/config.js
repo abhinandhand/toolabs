167df8b10fa88c5b3f45f5660a27f7a856f9a37f
adevadiga <abhinandhan.d@gmail.com>
2022-05-07 05:44:30 +0200

    Added staic site

---
+++ static/home/config.js
@@ -0,0 +1,10 @@
+IDRViewer.config = {"pagecount":16,"title":"PowerPoint Presentation","author":"Muse","subject":"","keywords":"","creator":"Microsoft® PowerPoint® 2019","producer":"Microsoft® PowerPoint® 2019","creationdate":"D:20211123221541+05'30'","moddate":"D:20211123221541+05'30'","trapped":"","fileName":"Toolabs_-_Profile.pdf","bounds":[[1466,825],[1466,825],[1466,825],[1466,825],[1466,825],[1466,825],[1466,825],[1466,825],[1466,825],[1466,825],[1466,825],[1466,825],[1466,825],[1466,825],[1466,825],[1466,825]],"bookmarks":[],"thumbnailType":"jpg","pageType":"html","pageLabels":[]};
+
+function scrollAbove(){
+    let main = document.getElementById('idrviewer');
+    main.scrollTop = 0 ;
+}
+
+function scrollToContactUs() {
+    document.getElementById('page16').scrollIntoView();
+}
